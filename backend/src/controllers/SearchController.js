acc89ee83fed3ef10f0fa6c2b520602aa980cb5a
Henrique Ramos <henriqueramos0106@gmail.com>
2020-08-25 23:21:22 -0300

    Adding complete project to Github

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,33 @@
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+    async index(resquest, response) {
+        /**
+         * Buscar todos os Devs num raio de 10km.
+         * Filtrar por tecnologias
+         */
+
+        const { latitude, longitude, techs } = resquest.query;
+        const techsArray = parseStringAsArray(techs);
+
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                    $maxDistance: 10000,
+                },
+
+            },
+        });
+        
+        return response.json({ devs });
+    }
+}
---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes');
+const http = require('http');
+const { setupWebsocket } = require('./websocket');
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect('mongodb+srv://hramos:hramos1234@cluster0-hcuzl.mongodb.net/semanaominstack10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+server.listen(3333);
